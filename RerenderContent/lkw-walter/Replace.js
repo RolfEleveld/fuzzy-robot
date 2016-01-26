4429cb36dcdff3bebb838f0a2783f35fb55598d3
Rolf Eleveld <rolf@bajomero.com>
2016-01-26 19:50:45 +0100

    Updated for LKW Walter
    Needs revisit on Portal Page

--- RerenderContent/lkw-walter/Replace.js
+++ RerenderContent/lkw-walter/Replace.js
@@ -31,43 +31,60 @@ function secondfrequencies() {
     $('span.mp-crwpRangeHistogramBar')[19].style.height = "calc(55% - 2px)";
 }
 function customize() {
-    //load the style
-    //var styleSheet: HTMLElement = document.createElement('STYLE');
-    //styleSheet.setAttribute("type",'text/css');
-    //styleSheet.setAttribute("rel", 'stylesheet');
-    //styleSheet.setAttribute("href", 'https://raw.githubusercontent.com/RolfEleveld/fuzzy-robot/master/RerenderContent/CamionTransport/Theme.css');
-    //document.head.appendChild(styleSheet);
     //replace menu icons
+
     //replace page icon
     document.body.innerHTML = document.body.innerHTML.replace(/\/_layouts\/15\/images\/Colygon\.MatchPoint\.Snow\/favicon\.ico/g, "http://www.camiontransport.ch/favicon.ico");
     //replace colors:
     for (var count = 0; count < document.styleSheets.length; count++) {
         document.styleSheets[count].href;
-        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255, 134, 31\)/g, 'rgb(170, 43, 62)');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255, 134, 31\)/g, '#00529E');        
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Segoe UI Light",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Segoe UI",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Lucida Grande",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Tahoma,/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Helvetica,Arial,sans-serif/g, 'Arial,Tahoma,sans-serif');
     }
+    
+    /*@media screen, projection*/
+
+    //Background
+    document.styleSheets[0].cssText = document.styleSheets[0].cssText + "\nbody{background: #00529E;overflow-y: scroll;z-index: 1;background-image: url('http://static.lkw-walter.ch/static/media/images/hintergrund-verlauf.png');position: relative;background-repeat: repeat-x;background-position: top left;}"
+    //rounded corners
+    /*@media screen, projection*/
+    // ".topradius7 {border-radius: 7px 7px 0 0;position: relative;}""
+
+
     //replace logo
-    $('#mpsnow-Logo > img').attr('src', "http://www.camiontransport.ch/portaldata/1/images/logo_symbol.gif");
+    $('#mpsnow-Logo > img').attr('src', "http://www.lkw-walter.ch/-/m/images/lkw/allgemein/lkw-walter-logo-slogan.ashx?h=50&amp;la=en&amp;w=200&amp;nocompress=1");
+    $('div.ms-breadcrumb-top').attr('style', "background-color:#00529E;");
+    $('#mpsnow-currentUser-Link').attr('style', "color:white;");
+    
+
     //replace words
-    document.body.innerHTML = document.body.innerHTML.replace(/gmz/g, 'Camion Transport').replace(/GMZ/g, 'Camion Transport');
-    document.body.innerHTML = document.body.innerHTML.replace(/migros/g, 'CT').replace(/GMZ/g, 'Camion Transport');
+    document.body.innerHTML = document.body.innerHTML.replace(/gmz/g, 'LKW-Walter').replace(/GMZ/g, 'LKW-Walter');
+    document.body.innerHTML = document.body.innerHTML.replace(/migros/g, 'LKW-Walter');
     // hashtags
     // Workspaces
     document.body.innerHTML = document.body.innerHTML.replace(/Community/g, 'On The Road');
     //Setting people images
-    document.body.innerHTML = document.body.innerHTML.replace(/vc0065\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmickey\.mouse\&amp\;size\=M/g, 'media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/081/1b2/095ace7.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/Aleksandar Toskovic/g, 'Josef A. Jäger');
-    document.body.innerHTML = document.body.innerHTML.replace(/vc0065\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5caleksandar\.toskovic\&amp\;size\=M/g, 'x1.xingassets.com/image/a_5_f_5e5312c87_23268962_1/josef-jäger-foto.256x256.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/Mickey Mouse/g, 'Reto Schenk');
+    document.body.innerHTML = document.body.innerHTML.replace(/vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmickey\.mouse\&amp\;size\=M/g, 'media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/000/12f/1e8/139b87f.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/Aleksandar Toskovic/g, 'Leo Hintersteiner');
+    document.body.innerHTML = document.body.innerHTML.replace(/vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5caleksandar\.toskovic\&amp\;size\=M/g, 'media.licdn.com/mpr/mpr/shrinknp_400_400/p/1/000/0bd/0ff/0825ad0.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/Mickey Mouse/g, 'Vincent Beaufils');
+    document.body.innerHTML = document.body.innerHTML.replace(/vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmaria\.studer\&amp\;size\=M/g, 'x1.xingassets.com/img/users/c/e/2/608a94f06.6034810,2.256x256.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/[Mm]aria.[Ss]tuder/g, 'Simon Friedl');
     // Content TimeLine
     if ($('#pageTitle').text().trim() === "Timeline") {
         firstfrequencies();
         $('.mpsnow-feed-RowContainer')[0].remove();
         // images on content
-        document.body.innerHTML = document.body.innerHTML.replace(/http[^"]+635881808960000000/g, 'http://www.camiontransport.ch/PortalData/1/Resources/bilder/aktuelles/CT_90Anni_01.jpg');
-        document.body.innerHTML = document.body.innerHTML.replace(/75 Jahr/g, '90 Jahr');
-        document.body.innerHTML = document.body.innerHTML.replace(/das Fitnessunternehmen Inline\/Injoy/g, 'der Jost Transport GmbH, Rümlang');
-        document.body.innerHTML = document.body.innerHTML.replace('Die Camion Transport-Tochtergesellschaft Migros Freizeit Deutschland GmbH übernimmt die deutsche Firma INLINE Beratung für Fitnessanlagen', 'CAMION TRANSPORT AG (CT) übernimmt die Bereiche Transport und Logistik der Jost Transport GmbH in Rümlang per 1. Januar 2016.');
-        document.body.innerHTML = document.body.innerHTML.replace(/http[^"]+635881805010000000/g, 'http://www.camiontransport.ch/PortalData/1/Resources/bilder/aktuelles/CT_JostTransporte.jpg');
+        document.body.innerHTML = document.body.innerHTML.replace(/http[^"]+635881808960000000/g, 'http://www.lkw-walter.ch/-/m/images/lkw/ueber-uns/lkw-walter-zentrale.ashx?h=160&amp;la=de&amp;mw=300&amp;w=300');
+        document.body.innerHTML = document.body.innerHTML.replace(/75 Jahre LKW-Walter./g, '30 year bearer of the Austrian state emblem');
+        document.body.innerHTML = document.body.innerHTML.replace(/>Happy Birthday[^<]+</g, '>Leo justo. Quis accumsan per porta hac. Porttitor iaculis viverra condimentum ridiculus nascetur Velit velit est euismod orci porta libero commodo dignissim nascetur convallis vulputate, facilisi tellus metus suspendisse pellentesque maecenas convallis, per. Class luctus. Ad cras per nisl tempor lacinia duis.<');
+        document.body.innerHTML = document.body.innerHTML.replace(/LKW-Walter-Tochtergesellschaft übernimmt das Fitnessunternehmen Inline\/Injoy/g, 'LKW-Walter green transport');
+        document.body.innerHTML = document.body.innerHTML.replace(/>Die LKW-Walter-Tochtergesellschaft[^<]+</g, '>Imperdiet hac volutpat. Tempor neque at dui libero lobortis congue a dictum conubia natoque malesuada non eget. Urna pede integer platea ligula bibendum lobortis congue nonummy, lacinia viverra interdum ac dis class auctor nam. Feugiat magna ultrices a. Sit ornare.<');
+        document.body.innerHTML = document.body.innerHTML.replace(/http[^"]+635881805010000000/g, 'http://www.lkw-walter.ch/-/m/images/lkw/ueber-uns/header/lkw-walter-green-transport-header.ashx?h=160&amp;la=de&amp;mw=300&amp;w=300');
     }
     // Workspaces
     if ($('#pageTitle').text().trim() === "Workspaces") {
