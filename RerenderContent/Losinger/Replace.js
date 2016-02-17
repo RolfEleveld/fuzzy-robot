21231c978f12ae729350f314118086d87f5b9068
Rolf Eleveld <rolf@bajomero.com>
2016-02-17 19:16:21 +0100

    added file for losinger

---
+++ RerenderContent/Losinger/Replace.js
@@ -0,0 +1,130 @@
+/*
+//Load this script
+$.getScript("./Replace.js")
+*/
+function firstfrequencies() {
+    // set frequency bar heights
+    $('span.mp-crwpRangeHistogramBar')[0].style.height = "calc(45% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[1].style.height = "calc(55% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[2].style.height = "calc(65% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[3].style.height = "calc(75% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[4].style.height = "calc(85% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[5].style.height = "calc(95% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[6].style.height = "calc(85% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[7].style.height = "calc(95% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[8].style.height = "calc(85% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[9].style.height = "calc(35% - 2px)";
+}
+function secondfrequencies() {
+    //second frequencies
+    $('span.mp-crwpRangeHistogramBar')[10].style.height = "calc(25% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[11].style.height = "calc(35% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[12].style.height = "calc(55% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[13].style.height = "calc(55% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[14].style.height = "calc(45% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[15].style.height = "calc(55% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[16].style.height = "calc(95% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[17].style.height = "calc(75% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[18].style.height = "calc(85% - 2px)";
+    $('span.mp-crwpRangeHistogramBar')[19].style.height = "calc(55% - 2px)";
+}
+function customize() {
+    //replace page icon
+    document.body.innerHTML = document.body.innerHTML.replace(/\/_layouts\/15\/images\/Colygon\.MatchPoint\.Snow\/favicon\.ico/g, "http://www.losinger-marazzi.ch/favicon.ico");
+    //replace colors:
+    for (var count = 0; count < document.styleSheets.length; count++) {
+        //Accent color        
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255, 134, 31\)/g, '#fdc400');
+        // background buttons: #605e5b, selected menu background #9a978f 
+        // bachground rest: #FFF
+        // Fonts
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Segoe UI Light",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Segoe UI",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Lucida Grande",/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Tahoma,/g, '');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Helvetica,Arial,sans-serif/g, '"corporateRoman","Helvetica Neue",Helvetica,Arial,sans-serif;');        
+        // Right Sidebar
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-webPartZoneRightSection[ \t\n\r]*{[ \t\n\r]*background-color[^}]+}/g, 'mpsnow-webPartZoneRightSection{background-color:#605e5b}');
+        // Left Menu color
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2;background-color:#f1f1f1;color:#44423e;}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)border-left[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)border-right[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2}');
+        // Menu
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-TopNavigationNode[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, 'mpsnow-TopNavigationNode{\\$1\\$2;background-color:#FFF;color:#000;}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#s4-titlerow[ \t\n\r]*{([^}]*)border-bottom-color[^;}]+([^}]*)}/g, '#s4-titlerow{\\$1\\$2;border-bottom-color:#FFF;}');
+        // Text on menu
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(85, 85, 85\)/g, '#44423e;');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(67, 67, 67\)/g, '#FFF');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/ms-breadcrumb-top[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, 'ms-breadcrumb-top{\\$1\\$2;background-color:#FFF;color:#000;}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-currentUser[ \t\n\r]+a[ \t\n\r]*{[ \t\n\r]*color[^;}]+([^}]*)}/g, 'mpsnow-currentUser a{\\$1;color:#000;}');
+        // Portal highlights:
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/background-color:[ \t\n\r]*#ffcc99[^;]/g, 'background-color:#f1f1f1');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255,[ \t\n\r]*204,[ \t\n\r]*153\)/g, '#f1f1f1');
+             
+    }
+    //replace logo
+    $('#mpsnow-Logo > img').attr('src', "http://www.losinger-marazzi.ch/media/global/img/layout/Losinger-Marazzi.png");
+
+    // Any left over words
+    document.body.innerHTML = document.body.innerHTML.replace(/[Gg][Mm][Zz]/g, 'Losinger-Marazzi')
+    document.body.innerHTML = document.body.innerHTML.replace(/migros/g, 'Losinger-Marazzi')
+    // hashtags
+    // Workspaces
+    //Setting people images
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5caleksandar\.toskovic\&amp\;size\=M/g, 'http://allgemeinebauzeitung.de/media/temp/csm_pv_18194_-_1sp_34378bae6d.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/Aleksandar Toskovic/g, 'Stefan Heissler');
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmickey\.mouse\&amp\;size\=M/g, '');
+    document.body.innerHTML = document.body.innerHTML.replace(/Mickey Mouse/g, 'Uwe Rechtsteiner');
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmaria\.studer\&amp\;size\=M/g, '');
+    document.body.innerHTML = document.body.innerHTML.replace(/[Mm]aria.[Ss]tuder/g, 'Andreas Böhm');
+    document.body.innerHTML = document.body.innerHTML.replace(/Ralph.Keller/g, 'Denis Zosso');
+
+ //TODO:: pull from need on Losinger-Marazzi site
+    // images on content
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635884501730000000"/g, '"http://www.losinger-marazzi.ch/shared/media/corporate/news/news-2016/02/11/Losinger-Marazzi-news-national-apprenticeship_img_310.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/Tanzwerk101/g, 'Awards');
+    document.body.innerHTML = document.body.innerHTML.replace(/Das finde ich super, bin dabei/g, 'Class luctus. Ad cras per nisl tempor lacinia duis.');
+    document.body.innerHTML = document.body.innerHTML.replace(/Der Tag der offenen Tanztür steht an - ein Tanzfest für Jedermann./g, 'Quis accumsan per porta hac.');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881808960000000"/g, '"http://www.losinger-marazzi.ch/shared/media/corporate/news/news-2016/02/09/Losinger-Marazzi-clean-sky2-itd-launch-review-participants-nov2015-72dpi_img_310.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/75.Jahre.Losinger-Marazzi./g, 'Events');
+    document.body.innerHTML = document.body.innerHTML.replace(/>[^<]*Happy Birthday[^<]*</g, '>Leo justo. Quis accumsan per porta hac. Porttitor iaculis viverra condimentum ridiculus nascetur Velit velit est euismod orci porta libero commodo dignissim nascetur convallis vulputate, facilisi tellus metus suspendisse pellentesque maecenas convallis, per. Class luctus. Ad cras per nisl tempor lacinia duis.<');
+    document.body.innerHTML = document.body.innerHTML.replace(/Losinger-Marazzi-Tochtergesellschaft übernimmt das Fitnessunternehmen Inline\/Injoy/g, 'Material');
+    document.body.innerHTML = document.body.innerHTML.replace(/>Die Losinger-Marazzi-Tochtergesellschaft[^<]+</g, '>Imperdiet hac volutpat. Tempor neque at dui libero lobortis congue a dictum conubia natoque malesuada non eget. Urna pede integer platea ligula bibendum lobortis congue nonummy, lacinia viverra interdum ac dis class auctor nam. Feugiat magna ultrices a. Sit ornare.<');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881805010000000"/g, '"http://www.losinger-marazzi.ch/shared/media/corporate/news/news-2016/02/08/Losinger-Marazzi-lr-11000-hanchang-handover2-72dpi_img_220.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635747988240000000"/g, '"http://www.losinger-marazzi.ch/shared/media/corporate/news/news-2016/02/09/Losinger-Marazzi-clean-sky2-itd-launch-review-participants-nov2015_img_710.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881812330000000"/g, '"http://www.losinger-marazzi.ch/shared/media/corporate/news/news-2016/01/27/gesch%C3%A4ftsentwicklung/Losinger-Marazzi-bauma-2013-72dpi_img_220.jpg"');
+
+    // Content TimeLine
+    if ($('#pageTitle').text().trim() === "Timeline") {
+        firstfrequencies();
+        $('.mpsnow-feed-RowContainer')[0].remove();
+    }
+    // Workspaces
+    if ($('#pageTitle').text().trim() === "Workspaces") {
+        firstfrequencies();
+        secondfrequencies();
+    }
+    if ($('#pageTitle').text().trim() === "Content") {
+        firstfrequencies();
+        secondfrequencies();
+    }
+    if ($('#pageTitle').text().trim() === "People") {
+    }
+    if ($('#pageTitle').text().trim() == "News") {
+    }
+    //Potal?
+    $("div.slick-track img").each(function (index) {
+        switch (index) {
+            case 1:
+                this.src = "http://demo.cloudimg.io/s/crop/1024x380/http://www.burkhalter.ch/img/catalogue/0/117/O_14fbb217-33a4-4d40-9544-a275d32b7abe.jpg";
+                break;
+        }
+    });
+    //replace colors:
+    for (var count = 0; count < document.styleSheets.length; count++) {
+        // Portal highlights:
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/background-color:[ \t\n\r]*#ffcc99[^;]/g, 'background-color:#f1f1f1');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255,[ \t\n\r]*204,[ \t\n\r]*153\)/g, '#f1f1f1');
+    }
+}
+customize();
