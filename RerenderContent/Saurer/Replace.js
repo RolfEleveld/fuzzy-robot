68db8a3c3ef95e2da2d2069785aeaabc55e960a1
Rolf Eleveld <rolf@bajomero.com>
2016-03-04 20:19:28 +0100

    Added Saurer

--- RerenderContent/Saurer/Replace.js
+++ RerenderContent/Saurer/Replace.js
@@ -34,7 +34,7 @@ function customize() {
     //replace colors:
     for (var count = 0; count < document.styleSheets.length; count++) {
         //Accent color        
-        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255, 134, 31\)/g, '#c10000');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255, 134, 31\)/g, '#d70000');
         // background buttons: #605e5b, selected menu background #9a978f 
         // bachground rest: #FFF
         // Fonts
@@ -42,21 +42,28 @@ function customize() {
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Segoe UI",/g, '');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/"Lucida Grande",/g, '');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Tahoma,/g, '');
-        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Helvetica,Arial,sans-serif/g, '"FrutigerLTW01-45Light","Arial","Helvetica",sans-serif;');        
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/Helvetica,Arial,sans-serif/g, 'Arial,Helvetica,sans-serif;');        
+        
         // Right Sidebar
-        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-webPartZoneRightSection[ \t\n\r]*{[ \t\n\r]*background-color[^}]+}/g, 'mpsnow-webPartZoneRightSection{background-color:#4e4644; color:#000}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-webPartZoneRightSection[ \t\n\r]*{[ \t\n\r]*background-color[^}]+}/g, 'mpsnow-webPartZoneRightSection{background:url("http://saurer.com/fileadmin/user_upload/Bilder-Alle/Key_visuals/2015_web-KV-Volkmann.jpg") repeat-y #d70000; color:#FFF}');
+             
         // Left Menu color
-        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2;background-color:#e6e7e8;color:#444444;}');
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2;background-color:#FFF;color:#555555;}');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)border-left[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2}');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#sideNavBox[ \t\n\r]*{([^}]*)border-right[^;}]+([^}]*)}/g, '#sideNavBox{\\$1\\$2}');
+
         // Menu
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-TopNavigationNode[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, 'mpsnow-TopNavigationNode{\\$1\\$2;background-color:#FFF;color:#000;}');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/#s4-titlerow[ \t\n\r]*{([^}]*)border-bottom-color[^;}]+([^}]*)}/g, '#s4-titlerow{\\$1\\$2;border-bottom-color:#FFF;}');
+        // Menu-separator
+        document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-group-separator[ \t\n\r]+a[ \t\n\r]*{[ \t\n\r]*border-top-color[^;}]+([^}]*)}/g, 'mpsnow-group-separator{\\$1;border-top-color:#b4b4b4;}');
+
         // Text on menu
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(85, 85, 85\)/g, '#44423e;');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(67, 67, 67\)/g, '#FFF');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/ms-breadcrumb-top[ \t\n\r]*{([^}]*)background-color[^;}]+([^}]*)}/g, 'ms-breadcrumb-top{\\$1\\$2;background-color:#FFF;color:#000;}');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/mpsnow-currentUser[ \t\n\r]+a[ \t\n\r]*{[ \t\n\r]*color[^;}]+([^}]*)}/g, 'mpsnow-currentUser a{\\$1;color:#000;}');
+
         // Portal highlights:
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/background-color:[ \t\n\r]*#ffcc99[^;]/g, 'background-color:#d1d3d4');
         document.styleSheets[count].cssText = document.styleSheets[count].cssText.replace(/rgb\(255,[ \t\n\r]*204,[ \t\n\r]*153\)/g, '#d1d3d4');
@@ -70,32 +77,32 @@ function customize() {
     // hashtags
     // Workspaces
     //Setting people images
-    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5caleksandar\.toskovic\&amp\;size\=M/g, 'https://www.xing.com/image/e_9_4_97494704f_15492982_1/rené-uebelhart-foto.256x256.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/Aleksandar Toskovic/g, 'René Uebelhart');
-    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmickey\.mouse\&amp\;size\=M/g, 'http://www.friedrich-zeitschrift.de/cms/wp-content/uploads/philipp-koester-02-470x323.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/Mickey Mouse/g, 'Philipp Köstner');
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5caleksandar\.toskovic\&amp\;size\=M/g, 'https://www.zentralfest-rheinfelden.ch/sites/default/files/ok/ufraecht.png');
+    document.body.innerHTML = document.body.innerHTML.replace(/Aleksandar Toskovic/g, 'Ivan Lima');
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmickey\.mouse\&amp\;size\=M/g, 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/094/33f/3356d72.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/Mickey Mouse/g, 'Martin Folini');
+    document.body.innerHTML = document.body.innerHTML.replace(/Ralph.Keller/g, 'Arndt Hamann');
     //logged on user
-    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmaria\.studer\&amp\;size\=M/g, 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/000/000/28f/39b7864.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/[Mm]aria.[Ss]tuder/g, 'Martin Pfister');
-    document.body.innerHTML = document.body.innerHTML.replace(/Ralph.Keller/g, 'Hansrudolf Labhart');
+    document.body.innerHTML = document.body.innerHTML.replace(/https...vc[0-9]{4}\.virtualcorp\.ch\/snow\/_layouts\/15\/userphoto\.aspx\?accountName\=i\%3a0\%23\.w\%7cvirtualcorp\%5cmaria\.studer\&amp\;size\=M/g, 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAKiAAAAJDYzNWM5ZDIyLTVjNTYtNDBjNi1hYjRjLWRlNDcwYzQyYTQ3Mw.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/[Mm]aria.[Ss]tuder/g, 'Pia Terasa');
 
     // images on content
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635918225680000000"/g, '"http://www.saurer.com/de/Resources/experience/Kategorie-Bilder/Inspiration/Saurer-Kreativwelt-Inspiration-Teaser.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635918225680000000"/g, '"http://schlafhorst.saurer.com/typo3temp/_processed_/csm_Fig3_Zinser_351_Internet_1364ead620.jpg"');
     document.body.innerHTML = document.body.innerHTML.replace(/>[^<]*Herzlich willkommen in der Logistik Frische gekühlt.(<br \/>)*[^<]*</g, '>Leo justo. Quis accumsan per porta hac. Porttitor iaculis viverra condimentum ridiculus nascetur Velit velit est euismod orci porta libero commodo dignissim nascetur convallis vulputate, facilisi tellus metus suspendisse pellentesque maecenas convallis, per. Class luctus. Ad cras per nisl tempor lacinia duis.<');
-    document.body.innerHTML = document.body.innerHTML.replace(/>[^<]*Logistik Frische gekühlt[^<]*</g, '>PourMonBijou<');
+    document.body.innerHTML = document.body.innerHTML.replace(/>[^<]*Logistik Frische gekühlt[^<]*</g, '>Ibrahim Fibres Limited<');
 
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635884501730000000"/g, '"background: url("http://blog.saurer.com/de/wp-content/uploads/sites/2/2016/02/img065-2-240x180.jpg');
-    document.body.innerHTML = document.body.innerHTML.replace(/Tanzwerk101/g, 'Quilten');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635884501730000000"/g, '"background: url("http://blog.saurer.com/uploads/pics/2016_02_Morarjee_India_216x148.jpg');
+    document.body.innerHTML = document.body.innerHTML.replace(/Tanzwerk101/g, 'Morarjee Textiles buys first Zinser 72 manufactured in India');
     document.body.innerHTML = document.body.innerHTML.replace(/Das finde ich super, bin dabei/g, 'Class luctus. Ad cras per nisl tempor lacinia duis.');
     document.body.innerHTML = document.body.innerHTML.replace(/Der Tag der offenen Tanztür steht an - ein Tanzfest für Jedermann./g, 'Quis accumsan per porta hac.');
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881808960000000"/g, '"http://www.saurer.com/de/TeaserData-de/Saurer-Maschinen/Images/modulteaser_1300mdc.jpg"');
-    document.body.innerHTML = document.body.innerHTML.replace(/75.Jahre.Saurer./g, 'Community');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881808960000000"/g, '"http://www.saurer.com/uploads/pics/2015_11_Schlafhorst_Emateks_216x148_01.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/75.Jahre.Saurer./g, 'Emateks Turkey');
     document.body.innerHTML = document.body.innerHTML.replace(/>[^<]*Happy Birthday[^<]*</g, '>Leo justo. Quis accumsan per porta hac. Porttitor iaculis viverra condimentum ridiculus nascetur Velit velit est euismod orci porta libero commodo dignissim nascetur convallis vulputate, facilisi tellus metus suspendisse pellentesque maecenas convallis, per. Class luctus. Ad cras per nisl tempor lacinia duis.<');
-    document.body.innerHTML = document.body.innerHTML.replace(/Saurer-Tochtergesellschaft übernimmt das Fitnessunternehmen Inline\/Injoy/g, 'Nähen');
+    document.body.innerHTML = document.body.innerHTML.replace(/Saurer-Tochtergesellschaft übernimmt das Fitnessunternehmen Inline\/Injoy/g, 'ITM Istanbul 2016, Turkey');
     document.body.innerHTML = document.body.innerHTML.replace(/>Die Saurer-Tochtergesellschaft[^<]+</g, '>Imperdiet hac volutpat. Tempor neque at dui libero lobortis congue a dictum conubia natoque malesuada non eget. Urna pede integer platea ligula bibendum lobortis congue nonummy, lacinia viverra interdum ac dis class auctor nam. Feugiat magna ultrices a. Sit ornare.<');
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881805010000000"/g, '"http://www.saurer.com/de/TeaserData-de/Software/Images/modulteaser_designworks-de.jpg"');
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635883825300000000"/g, '"http://www.saurer.com/assetgen/4/de/Resources/company/company_header-png.jpg"');
-    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881812330000000"/g, '"http://www.saurer.com/de/TeaserData-de/Saurer-Maschinen/Images/modulteaser_580-png.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881805010000000"/g, '"http://www.saurer.com/uploads/pics/2015_11_Schlafhorst_Emateks_216x148_01.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635883825300000000"/g, '"http://www.texdata.com/content/texdata-oerlikon-saurer-allma-winder-tc2-899.jpg"');
+    document.body.innerHTML = document.body.innerHTML.replace(/"[^"]+635881812330000000"/g, '"http://www.textile-future.com/dynpg/upload/imgfile885.jpg"');
 
     // Content TimeLine
     if ($('#pageTitle').text().trim() === "Timeline") {
@@ -119,7 +126,7 @@ function customize() {
     $("div.slick-track img").each(function (index) {
         switch (index) {
             case 1:
-                this.src = "http://www.saurer.com/uploads/pics/geschichte.jpg";
+                this.src = "http://www.texdata.com/content/texdata-oerlikon-saurer-allma-winder-tc2-899.jpg";
                 break;
         }
     });
